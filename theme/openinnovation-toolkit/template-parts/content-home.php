3f4cc1d0a7dd3cd6d1e9544290987e9a3e3a06dd
emmairwin <emma.irwin@gmail.com>
2016-12-01 22:06:37 -0800

    styles

--- theme/openinnovation-toolkit/template-parts/content-home.php
+++ theme/openinnovation-toolkit/template-parts/content-home.php
@@ -7,7 +7,7 @@
   <div class="container">
     <div class="row">
       <div class="col-md-8 col-xs-12 col-md-offset-2">
-        <h2><?php the_title();?></h2>
+      
         <?php the_content();?>
       </div>
     </div>
